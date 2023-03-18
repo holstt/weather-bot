adaaf23335a6591f097ed659fe4683419e080687
roedebaron <45661813+roedebaron@users.noreply.github.com>
2023-03-18 19:49:45 +0100

    Small fixes pipeline, config

--- src/config.py
+++ src/config.py
@@ -6,9 +6,9 @@ class AppConfig:
     def __init__(self) -> None:
         self.ENV = env.get('WB_ENVIRONMENT')
         self.BOT_TOKEN = env.get('BOT_TOKEN')
-        self.DEV_CHANNEL_ID = int(env.get('DEV_CHANNEL_ID'))
-        self.DEV_GUILD_ID = int(env.get('DEV_GUILD_ID'))
-        self.TARGET_GUILD_ID = int(env.get('TARGET_GUILD_ID'))
-        self.LAT = float(env.get('LAT'))
-        self.LON = float(env.get('LON'))
-        self.TIME_ZONE = env.get('TIME_ZONE')
+        self.DEV_CHANNEL_ID = int(env['DEV_CHANNEL_ID'])
+        self.DEV_GUILD_ID = int(env['DEV_GUILD_ID'])
+        self.TARGET_GUILD_ID = int(env['TARGET_GUILD_ID'])
+        self.LAT = float(env['LAT'])
+        self.LON = float(env['LON'])
+        self.TIME_ZONE = env['TIME_ZONE']
